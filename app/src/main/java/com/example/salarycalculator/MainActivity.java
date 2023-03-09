1cd04b00c9f9c575f8017064c28bbde9cf91eab1
liri9 <lichow.liri@gmail.com>
2023-03-09 18:33:08 +0200

    added buttons functions

--- app/src/main/java/com/example/salarycalculator/MainActivity.java
+++ app/src/main/java/com/example/salarycalculator/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
 import com.example.salarycalculator.Calculator;
+
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     private MaterialButton main_BTN_net;
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private MaterialTextView main_LBL_gross;
     private MaterialTextView main_LBL_result;
     private MaterialTextView main_LBL_submitter;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,29 +38,67 @@ public class MainActivity extends AppCompatActivity {
         main_BTN_pension = findViewById(R.id.main_BTN_pension);
         main_BTN_income_tax = findViewById(R.id.main_BTN_income_tax);
         main_EDT_gross = findViewById(R.id.main_EDT_gross);
-        main_LBL_gross= findViewById(R.id.main_LBL_gross);
-        main_LBL_result= findViewById(R.id.main_LBL_result);
+        main_LBL_gross = findViewById(R.id.main_LBL_gross);
+        main_LBL_result = findViewById(R.id.main_LBL_result);
 
+
+        main_BTN_net.setOnClickListener(view -> calculateNet());
+        main_BTN_education_fund.setOnClickListener(view -> calculateEdu());
+        main_BTN_social_security.setOnClickListener(view -> calculateSocialSecurity());
+        main_BTN_health_tax.setOnClickListener(view -> calculateHealthTax());
+        main_BTN_pension.setOnClickListener(view -> calculatePension());
+        main_BTN_income_tax.setOnClickListener(view -> calculateIncomeTax());
+
+    }
+
+    private void calculateNet() {
         String grossSTR = main_EDT_gross.getText().toString();
-        int gross =  Integer.valueOf(grossSTR);
+        int gross = Integer.valueOf(grossSTR);
         Calculator current = new Calculator(gross);
-
         double net = current.getNet();
+        main_LBL_result.setText("Result: " + df.format(net));
+    }
+    private void calculateEdu() {
+        String grossSTR = main_EDT_gross.getText().toString();
+        int gross = Integer.valueOf(grossSTR);
+        Calculator current = new Calculator(gross);
         double EducationFund = current.getEducationFund();
+        main_LBL_result.setText("Result: " + df.format(EducationFund));
+    }
+
+
+
+    private void calculateIncomeTax() {
+        String grossSTR = main_EDT_gross.getText().toString();
+        int gross = Integer.valueOf(grossSTR);
+        Calculator current = new Calculator(gross);
         double incomeTax = current.getIncomeTax();
-        double socialSecurity =  current.getSocialSecurity();
-        double healthTax =current.getHealthTax();
-        double  pension =current.getPensionFund();
-
-        main_BTN_net.setOnClickListener(view -> calculate(net));
-        main_BTN_education_fund.setOnClickListener(view -> calculate(EducationFund));
-        main_BTN_social_security.setOnClickListener(view -> calculate(socialSecurity));
-        main_BTN_health_tax.setOnClickListener(view -> calculate(healthTax));
-        main_BTN_pension.setOnClickListener(view -> calculate(pension));
-        main_BTN_income_tax.setOnClickListener(view -> calculate(incomeTax));
-
+        main_LBL_result.setText("Result: " + df.format(incomeTax));
     }
-    private void calculate(double gross){
-        main_LBL_result.setText("Result: "+gross);
+    private void calculateSocialSecurity() {
+        String grossSTR = main_EDT_gross.getText().toString();
+        int gross = Integer.valueOf(grossSTR);
+        Calculator current = new Calculator(gross);
+        double socialSecurity = current.getSocialSecurity();
+        main_LBL_result.setText("Result: " + df.format(socialSecurity));
     }
+    private void calculateHealthTax() {
+        String grossSTR = main_EDT_gross.getText().toString();
+        int gross = Integer.valueOf(grossSTR);
+        Calculator current = new Calculator(gross);
+        double healthTax = current.getHealthTax();
+        main_LBL_result.setText("Result: " + df.format(healthTax));
+    }
+    private void calculatePension() {
+        String grossSTR = main_EDT_gross.getText().toString();
+        int gross = Integer.valueOf(grossSTR);
+        Calculator current = new Calculator(gross);
+        double pension = current.getPensionFund();
+        main_LBL_result.setText("Result: " + df.format(pension));
+    }
+    /*
+
+
+
+     */
 }
